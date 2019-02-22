4b61f53157774c8181529bf2284acdacd1f3b868
Songhae <shkwoak@daum.net>
2019-02-22 10:41:58 +0900

    Add files via upload

---
+++ 1-2. ExcelOpen.py
@@ -0,0 +1,9 @@
+import win32com.client
+
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = True
+wb = excel.Workbooks.Open('C:/Users/04869/Downloads/Pandas Study/data/korea_pop_2012.xls')
+ws = wb.ActiveSheet
+print(ws.Cells(3,3).Value)
+ws.cells(1,1).value = ws.Cells(3,3).Value + ws.Cells(4,4).Value
+# excel.Quit()
